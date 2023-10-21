ad2396ee5d870124b41c37e377b3ad733208042e
Pedro Henrique <pedro.henrike2812@gmail.com>
2023-10-21 15:01:27 -0300

    feat: add second banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -26,8 +26,18 @@ export default async function Home() {
       </div>
 
       <div className="mt-8">
+        <p className="mb-3 pl-5 font-bold uppercase">Ofertas</p>
         <ProductList products={deals} />
       </div>
+
+      <Image
+        src="/banner-home-02.png"
+        alt="até 55% de desconto em mouses"
+        height={0}
+        width={0}
+        sizes="100vw"
+        className="h-auto w-full px-5"
+      />
     </div>
   )
 }
