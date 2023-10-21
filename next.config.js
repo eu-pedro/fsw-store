82fcab9f85ede1959a73f7141826dc51cf468052
Pedro Henrique <pedro.henrike2812@gmail.com>
2023-10-21 14:41:17 -0300

    chore: add s3 domain to nextconfig file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    domains: [
+      'lh3.googleusercontent.com',
+      'fsw-store.s3.sa-east-1.amazonaws.com',
+    ],
   },
 }
 
